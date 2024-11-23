0b9384a8727dca9418c905b6dbd5f2401a189e5f
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2024-11-23 17:49:28 +0300

    Add project files.

---
+++ src/PublicApi/CatalogItemEndpoints/DeleteCatalogItemEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+
+namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints;
+
+/// <summary>
+/// Deletes a Catalog Item
+/// </summary>
+public class DeleteCatalogItemEndpoint(IRepository<CatalogItem> itemRepository) : Endpoint<DeleteCatalogItemRequest, Results<Ok<DeleteCatalogItemResponse>, NotFound>>
+{
+    public override void Configure()
+    {
+        Delete("api/catalog-items/{catalogItemId}");
+        Roles(BlazorShared.Authorization.Constants.Roles.ADMINISTRATORS);
+        AuthSchemes(JwtBearerDefaults.AuthenticationScheme);
+        Description(d =>
+            d.Produces<DeleteCatalogItemResponse>()
+             .WithTags("CatalogItemEndpoints"));
+    }
+
+    public override async Task<Results<Ok<DeleteCatalogItemResponse>, NotFound>> ExecuteAsync(DeleteCatalogItemRequest request, CancellationToken ct)
+    {
+        var response = new DeleteCatalogItemResponse(request.CorrelationId());
+
+        var itemToDelete = await itemRepository.GetByIdAsync(request.CatalogItemId, ct);
+        if (itemToDelete is null)
+            return TypedResults.NotFound();
+
+        await itemRepository.DeleteAsync(itemToDelete, ct);
+
+        return TypedResults.Ok(response);
+    }
+}
---
+++ src/PublicApi/Program.cs
@@ -0,0 +1,87 @@
+﻿using BlazorShared;
+using FastEndpoints;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.eShopWeb.Infrastructure;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+using Microsoft.eShopWeb.PublicApi;
+using Microsoft.eShopWeb.PublicApi.Extensions;
+using Microsoft.eShopWeb.PublicApi.Middleware;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire components.
+builder.AddAspireServiceDefaults();
+
+builder.Services.AddFastEndpoints();
+
+// Use to force loading of appsettings.json of test project
+builder.Configuration.AddConfigurationFile("appsettings.test.json");
+
+builder.Services.ConfigureLocalDatabaseContexts(builder.Configuration);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<AppIdentityDbContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.AddCustomServices(builder.Configuration);
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddJwtAuthentication();
+
+const string CORS_POLICY = "CorsPolicy";
+
+var configSection = builder.Configuration.GetRequiredSection(BaseUrlConfiguration.CONFIG_NAME);
+builder.Services.Configure<BaseUrlConfiguration>(configSection);
+var baseUrlConfig = configSection.Get<BaseUrlConfiguration>();
+builder.Services.AddCorsPolicy(CORS_POLICY, baseUrlConfig!);
+
+builder.Services.AddControllers();
+
+// TODO: Consider removing AutoMapper dependency (FastEndpoints already has its own Mapper support)
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+builder.Services.AddSwagger();
+
+var app = builder.Build();
+
+app.Logger.LogInformation("PublicApi App created...");
+
+await app.SeedDatabaseAsync();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(CORS_POLICY);
+
+app.UseAuthorization();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
+app.UseFastEndpoints();
+
+app.Logger.LogInformation("LAUNCHING PublicApi");
+app.Run();
+
+public partial class Program { }
